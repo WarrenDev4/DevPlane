84c008fc1aa556b4031cfca015a3757fc54f2808
Warrenn205 <warrenspencer41@gmail.com>
2025-06-05 21:38:04 -0500

    Added Sign Up and Sign In pages

---
+++ devplane/app/login/page.tsx
@@ -0,0 +1,6 @@
+import UserForm from '../components/UserForm/UserForm';
+
+export default function LoginPage() {
+  return <UserForm type="login" />;
+}
+
---
+++ devplane/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import UserForm from '../components/UserForm/UserForm';
+
+export default function SignupPage() {
+  return <UserForm type="signup" />;
+}
