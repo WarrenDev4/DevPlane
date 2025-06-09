3adc75c008cf979bdd4b98e9e27cddc93a643592
Warrenn205 <warrenspencer41@gmail.com>
2025-06-09 13:15:24 -0500

    Designing the Main Page

--- devplane/app/main-page/page.tsx
+++ devplane/app/main-page/page.tsx
@@ -4,9 +4,18 @@ import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
 
-export default function Homepage() {
-  return (
-    <div>
-    </div>
-  );
+export default function Home() {
+    return (
+        <div className="pageWrapper">
+            <header className="backgroundNavbar">
+                <div className="logo-nav">
+                <a href="/main-page"><img src="/DevPlane Logo (White).png" alt="Logo" width={220} height={60}/></a>
+                <nav className="nav">
+                    <Link href="/login" className="login-button">Login</Link>
+                    <Link href="/signup" className="signup-button">Sign Up</Link>
+                </nav> 
+                </div>
+            </header>
+        </div>
+    );
 }
